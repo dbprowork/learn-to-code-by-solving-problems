d2527b0c5a438e0ead6e1d109a2a9b4f033641be
Daniel Brown <121890999+dbprowork@users.noreply.github.com>
2024-01-12 08:51:55 +0000

    New file created - problem solved

---
+++ problem-1-word-count.py
@@ -0,0 +1,13 @@
+"""
+Problem #1: Word Count
+
+Count the number of words provided. For this problem, a word 
+is any sequence of lowercase letters. For example, hello is 
+a word, but so are non-English “words” like bbaabbb.
+
+DMOJ dmopc15c7p2
+"""
+
+# DMOJ submission (accepted):
+
+print(input().count(' ') + 1)
