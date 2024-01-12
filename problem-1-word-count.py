99f777e1c51a604a993f1f797b2381b5c7ec9305
Daniel Brown <121890999+dbprowork@users.noreply.github.com>
2024-01-12 17:20:09 +0000

    Format change

--- problem-1-word-count.py
+++ problem-1-word-count.py
@@ -1,9 +1,9 @@
 """
 Problem #1: Word Count
 
-Count the number of words provided. For this problem, a word 
-is any sequence of lowercase letters. For example, hello is 
-a word, but so are non-English “words” like bbaabbb.
+Count the number of words provided. For this problem, a word is any sequence of 
+lowercase letters. For example, hello is a word, but so are non-English “words” 
+like bbaabbb.
 
 DMOJ dmopc15c7p2
 """
