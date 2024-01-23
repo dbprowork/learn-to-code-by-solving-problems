fa5afff48a7460317bec3e25c5c337cb7a7b2c3e
Daniel Brown <121890999+dbprowork@users.noreply.github.com>
2024-01-23 21:41:58 +0000

    Create extra-04-hows-the-weather.py
    Problem solved

---
+++ extra-04-hows-the-weather.py
@@ -0,0 +1,8 @@
+# Output a single integer, the temperature in degrees Fahrenheit
+# which is equivalent to C degrees Celsius.
+
+# DMOJ wc17c1j2
+
+# DMOJ submission (accepted):
+
+print(int(input()) / (5 / 9) + 32)
